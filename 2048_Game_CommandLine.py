cbccfa039d7a97ddc4b0c1be0362500cdfdc7ff0
Ashutosh Kumar <71917947+AshutoshKY@users.noreply.github.com>
2021-10-05 22:50:50 +0530

    Add files via upload

---
+++ 2048_Game_CommandLine.py
@@ -0,0 +1,103 @@
+import random
+
+def start_game():
+    mat = []
+    for i in range(4):
+        mat.append([0]*4)
+    return mat
+
+def add_new_2(mat):
+    r=random.randint(0,3)
+    c=random.randint(0,3)
+    while mat[r][c]!=0:
+        r=random.randint(0,3)
+        c=random.randint(0,3)
+    mat[r][c]=2
+
+def compress(mat):
+    new_mat=[]
+    for i in range(4):
+        new_mat.append([0]*4)    
+    for i in range(4):
+        pos=0
+        for j in range(4):
+            if mat[i][j]!=0:
+                new_mat[i][pos]=mat[i][j]
+                pos=pos+1        
+    return new_mat
+
+def merge(mat):
+    for i in range(4):
+        for j in range(3):
+            if mat[i][j]==mat[i][j+1]  and mat[i][j]!=0     :
+                mat[i][j]=mat[i][j]*2
+                mat[i][j+1]=0
+    return mat
+
+def reverse(mat):
+    new_mat=[]
+    for i in range(4):
+        new_mat.append([])
+        for j in range(4):
+            new_mat[i].append(mat[i][4-j-1])
+    return new_mat
+
+def transpose(mat):
+    new_mat=[]
+    for i in range(4):
+        new_mat.append([])
+        for j in range(4):
+            new_mat[i].append(mat[j][i])
+    return new_mat
+
+def move_up(grid):
+    
+    transposed_grid=transpose(grid)
+    new_grid=compress(transposed_grid)
+    new_grid=merge(new_grid)
+    new_grid=compress(new_grid)
+    final_new_grid=transpose(new_grid)
+    return final_new_grid
+
+def move_down(grid):
+    transposed_grid=transpose(grid)
+    reversed_grid=reverse(transposed_grid)
+    new_grid=compress(reversed_grid)
+    new_grid=merge(new_grid)
+    new_grid=compress(new_grid)
+    final_reversed_grid=reverse(new_grid)
+    final_new_grid=transpose(final_reversed_grid)
+    return final_new_grid
+
+def move_right(grid):
+    reversed_grid=reverse(grid)
+    new_grid=compress(reversed_grid)
+    new_grid=merge(new_grid)
+    new_grid=compress(new_grid)
+    final_new_grid=reverse(new_grid)
+    return final_new_grid
+
+def move_left(grid):
+    new_grid=compress(grid)
+    new_grid=merge(new_grid)
+    new_grid=compress(new_grid)
+    return new_grid
+
+mat = start_game()
+mat[1][3] = 2
+mat[2][2] = 2
+mat[3][0] = 4
+mat[3][1] = 8
+mat[2][1] = 4
+
+inputs = [int(ele) for ele in input().split()]
+for ele in inputs:
+    if ele == 1:
+        mat = move_up(mat)
+    elif ele == 2:
+        mat = move_down(mat)
+    elif ele == 3:
+        mat = move_left(mat)
+    else:
+        mat = move_right(mat)
+    print(mat)
