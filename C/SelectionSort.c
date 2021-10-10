75d067acf2a5f8e6dcafc2af4d2eecc734e86752
satyam1906 <psatyam1618@gmail.com>
2021-10-10 20:01:50 +0530

    created SelectionSort

---
+++ C/SelectionSort.c
@@ -0,0 +1,49 @@
+//Program asks for Number of elements you want to give as input
+//Then it takes all the elements as input values and arranges them in ascending order using selection sort. 
+//It also prints the array you have given as input
+
+
+#include <stdio.h>
+#include <stdlib.h>
+void selection_sort(int A[],int n);
+
+int main()
+{
+  int a[50],i,j,n,t;
+
+  printf("how many element you want to input: ");
+  scanf("%d",&n);
+  printf("Now enter %d element one by one\n:",n);
+  for(i=0;i<n;i++)
+  {
+      scanf("%d",&a[i]);
+
+  }
+  printf("\n Your elements are:");
+  for(i=0;i<n;i++)
+  {
+      printf("%d",a[i]);
+
+  }
+  for(i=0;i<n-1;i++)
+  {
+      for(j=i+1;j<n;j++)
+      {
+          if(a[i]>a[j])
+          {
+              t = a[i];
+              a[i]=a[j];
+              a[j]=t;
+          }
+
+
+      }
+  }
+  printf("\nThe sorted elements in asscending order are:\n");
+  for(i=0;i<n;i++)
+  {
+      printf("%d",a[i]);
+
+  }
+
+}
